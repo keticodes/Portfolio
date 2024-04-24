1c9462fe23a56c0a5582c7b0e68b21628591cdb5
Keti <keti.mandunga@metropolia.fi>
2024-04-24 11:59:40 +0300

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>Your detailed bio goes here.</p>
+    </section>
+  );
+}
+
+export default About;
