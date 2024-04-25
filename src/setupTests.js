52b43b63be5e2e06f9db8df3dd4061a6fafb1525
Keti <keti.mandunga@metropolia.fi>
2024-04-25 09:34:15 +0300

    uodate

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// This optional code is used to configure or
